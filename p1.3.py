cf274090a51ad1b4cc3453ed2ae8461894423257
root <bakhtyari.mehdi@gmail.com>
2021-07-31 12:44:57 +0430

    tested

---
+++ p1.3.py
@@ -0,0 +1,5 @@
+number=int(input())
+x=(number//10)
+y=x+1
+z=10*y
+print(z)
---
+++ p1.5.py
@@ -0,0 +1,13 @@
+old_years=int(input())
+if 0<old_years<6:
+    print('khordsal')
+elif 6<=old_years<10:
+    print('koodak')
+elif 10<=old_years<14:
+    print('nojavan')
+elif 14<=old_years<24:
+    print('javan')
+elif 24<=old_years<40:
+    print('bozorgsal')
+elif 40<=old_years:
+    print('miansal')
