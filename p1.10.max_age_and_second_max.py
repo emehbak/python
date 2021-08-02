19c2f4c73b438740608f7dfcc8ae4223f3b607bc
Mehdi Bakhtyari <73363479+emehbak@users.noreply.github.com>
2021-08-03 00:28:27 +0430

    tested

--- p1.10.max_age_and_second_max.py
+++ p1.10.max_age_and_second_max.py
@@ -1,1 +1,13 @@
-s
+#to find out the maximum age and second maximum age
+age1=0
+age2=0
+input_age=int(input())
+while input_age!=-1:
+    if input_age>age1:
+        age2=age1
+        age1=input_age
+    elif input_age>age2:
+        age2=input_age
+    input_age=int(input())
+
+print(age1,age2)
