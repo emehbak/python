929e97a411bf482f51352dcc6ba7ce9a90a6be82
root <bakhtyari.mehdi@gmail.com>
2021-08-02 14:51:47 +0430

    a game guess/what

---
+++ p1.5game.py
@@ -0,0 +1,20 @@
+def game():
+    import random
+    javab=random.randint(1,99)
+    name=input('Good:) , what is your name? ')
+    print('OK ',name,' lets start...')
+    hads=int(input('eneter a number between 1 and 99: '))
+    while hads!=javab:
+            if hads>javab:
+                print('give smaller number ')
+            elif hads<javab:
+                print('give bigger number ')
+            hads=int(input('eneter a number between 1 and 99: '))
+    print(hads,' correct !... awesome :) ')
+
+answer=str(input('do you want to play a "guess what" game? (y/n): '))
+if answer=='y':
+    game()
+elif answer=='n':
+    print('OK, Thnaks:)')
+
