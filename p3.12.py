76cd37f3b5a2aba5385a21d4fa435f84cd5999ed
Mehdi Bakhtyari <73363479+emehbak@users.noreply.github.com>
2021-08-10 13:12:42 +0430

    comments added

--- p3.12.py
+++ p3.12.py
@@ -1,11 +1,11 @@
 #dictionary , it get 5 input and interprets to correspondence 
 load=dict()
-n=int(input())
+n=int(input())  #enter the number words to translate
 for i in range(0,n):
     x=input().split()
     load[x[0]]=x[1]
 
-y=input().split()
+y=input().split()  #enter the sentence
 z=''
 for i in y:
     z=z+' '+load.get(i,i)
